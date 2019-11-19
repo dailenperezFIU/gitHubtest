67eebfc1cb6d0a7aff5493d6afaef65912a991b9
Dailen Perez <43188007+dailenperezFIU@users.noreply.github.com>
2019-11-19 12:16:42 -0500

    Update TestingGitHub.java

--- testingGitHub/src/testinggithub/TestingGitHub.java
+++ testingGitHub/src/testinggithub/TestingGitHub.java
@@ -17,7 +17,7 @@ public class TestingGitHub {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Removed all");
-        
+         System.out.println("pull test");
       
         
         
